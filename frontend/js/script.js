58e495d34e3d3349b005931b2817eba59381b077
Nelson-arantes <nelson-arantes2@outlook.com>
2024-01-19 11:20:55 -0300

    main

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -70,7 +70,7 @@ const handleLogin = (event)=>{
     user.cor = getRandomColor()
     login.style.display = "none"
     chat.style.display = "flex"
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://chat-x9d9.onrender.com")
     websocket.onmessage = processMessage
 }
 
